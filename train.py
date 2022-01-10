31a235b47fc166384683eb2f23f6c5bceaf3bf94
Shashikanth G K <shashikanthgk99@gmail.com>
2022-01-10 23:23:28 +0530

    adding data

--- train.py
+++ train.py
@@ -62,7 +62,7 @@ def main(args):
             word_embed_size=args.word_embed_size,
             num_layers=args.num_layers,
             hidden_size=args.hidden_size,
-            num_attention_layer = num_attention_layer).to(device)
+            num_attention_layer = args.num_attn_layer).to(device)
     else:
         print("No specific model is mentioned! Aborting ...... !!!")
         exit(0)
